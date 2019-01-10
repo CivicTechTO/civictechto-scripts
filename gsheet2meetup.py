98ce0b57ba3ef7933c80e9ddf1a5978109fdfaee
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-01-10 03:32:28 -0500

    Set meetup event description from template_url.

--- gsheet2meetup.py
+++ gsheet2meetup.py
@@ -2,6 +2,7 @@ import click
 import csv
 import datetime
 import meetup.api
+import pystache
 import re
 import requests
 
@@ -61,41 +62,40 @@ def gsheet2meetup(meetup_api_key, gsheet_url, meetup_group_slug):
             gevent_start = gevent_start + datetime.timedelta(hours=int(h), minutes=int(m))
             # Ignore non-sync'd or past events
             is_past = lambda d: d < datetime.datetime.now()
-            if not row['do_sync'] or is_past(gevent_start):
+            if not row['do_update'] or is_past(gevent_start):
                 continue
             get_mevent_start = lambda ev: datetime.datetime.fromtimestamp(ev.get('time')/1000)
             [this_event] = [ev for ev in meetup_events if get_mevent_start(ev).date() == gevent_start.date()]
             if this_event:
                 print(this_event)
                 print(row)
+                # Skip event if not ending in two asterisks "**".
+                # We are using this string as a flag for which events to manage.
+                is_event_managed = lambda ev: ev['simple_html_description'].endswith('**')
+                if not is_event_managed(this_event):
+                    print("Event found, but not managed by this script... (event description not ending in '**')")
+                    continue
+
+                r = requests.get(row['template_url'])
+                desc_tmpl = r.text
+                # Pass spreadsheet dict into template func, do token replacement via header names.
+                desc = pystache.render(desc_tmpl, row)
+
+                # TODO: set image
+
+                # TODO: set venue
+
+                # TODO: set organizers
+
                 event_data = {
                     'urlname': meetup_group_slug,
                     'id': this_event['id'],
                     'name': row['title_full'],
+                    'rsvp_limit': row['rsvp_limit'],
+                    # Pass spreadsheet dict into template func, do token replacement via header names.
+                    'description': desc,
                 }
                 response = mclient.EditEvent(**event_data)
-                print(response)
-
-    # parse gsheet event data
-
-    # get meetup.com events
-
-    # foreach gsheet event
-
-        # if exists on meetup.com
-
-            # if flag for management doesn't exist
-
-                # skip
-
-            # update event
-
-        # else
-
-            # select random organizers
-
-            # create new event
-    pass
 
 if __name__ == '__main__':
     gsheet2meetup(auto_envvar_prefix='CTTO')
