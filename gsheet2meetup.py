2a5fb39de522d3bb1598864e630426c7f57eef7d
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-01-16 13:56:55 -0500

    gsheet2meetup: Cleanup. Allow venue to be set.

--- gsheet2meetup.py
+++ gsheet2meetup.py
@@ -97,11 +97,6 @@ def gsheet2meetup(meetup_api_key, gsheet_url, meetup_group_slug, yes, debug):
             # TODO: if event doesn't exist, create
             # If event exists, update
             if this_event:
-                if debug:
-                    click.echo('MATCHED MEETUP EVENT')
-                    click.echo(pprint.pformat(this_event))
-                    click.echo('SPREADSHEET EVENT DATA')
-                    click.echo(pprint.pformat(row))
                 # Skip event if not ending in two asterisks "**".
                 # We are using this string as a flag for which events to manage.
                 is_event_managed = lambda ev: ev['simple_html_description'].endswith('**')
@@ -110,84 +105,103 @@ def gsheet2meetup(meetup_api_key, gsheet_url, meetup_group_slug, yes, debug):
                     click.echo(skip_event_msg.format(date=row['date']))
                     continue
 
-                template_url = "{url}?r={cachebuster}".format(url=row['template_url'], cachebuster=script_time)
-                r = requests.get(template_url)
-                desc_tmpl = r.text
-                # Pass spreadsheet dict into template func, do token replacement via header names.
-                desc = pystache.render(desc_tmpl, row)
-                # Add "**" to end of description to indicate this event as managed.
-                desc = desc + "\n\n**"
-
-                # Download image.
-                _, image_path = tempfile.mkstemp()
-                r = requests.get(row['image_url'], allow_redirects=True)
-                f = open(image_path, 'wb')
-                f.write(r.content)
-                featured_image = open(image_path, 'rb')
-
-                r = mclient.GetPhotoAlbums(group_urlname=meetup_group_slug)
-                [album] = [a for a in r.items if a['title'] == 'Meetup Group Photo Album']
-                r = mclient.GetPhotos(photo_album_id=album['id'])
-                # TODO: Download image from url.
-                image_data = mclient.CreateGroupPhoto(await=True, group_urlname=meetup_group_slug, main=False, photo=featured_image)
-                photo_id = image_data.group_photo_id
-                # Set a caption so we can know which to delete on change.
-                identifying_caption = 'Featured image for {date} event'.format(date=row['date'])
-                new_photo = mclient.EditPhoto(id=photo_id, caption=identifying_caption)
-                if debug: click.echo(pprint.pformat(new_photo.__dict__))
-                # Delete photos that are similarly-marked for this event.
-                r = mclient.GetPhotos(photo_album_id=album['id'])
-                marked_photos = [p for p in r.results if p.get('caption') == identifying_caption]
-                # Skip the one we just uploaded if it's present.
-                for photo in [p for p in marked_photos if p['photo_id'] != new_photo.photo_id]:
-                    r = mclient.DeletePhoto(id=photo['photo_id'])
-                    if debug: click.echo(r.message)
-
-                r = mclient.GetVenues(group_urlname=meetup_group_slug)
-                recent_venues = r.results
-                venue = None
-                # search prior venues for exact name match
-                matched_venues = [v for v in recent_venues if row['venue_name'] == v['name']]
-                if matched_venues:
-                    venue = matched_venues.pop()
-                # search prior venues for partial name match
-                if not venue:
-                    matched_venues = [v for v in recent_venues if row['venue_name'] in v['name']]
-                    if matched_venues:
-                        venue = matched_venues.pop()
-
-                # search open public venues for matches
-                # TODO: test this and enable
-                feature_enabled = False
-                if not venue and feature_enabled:
-                    r = mclient.GetOpenVenues(group_urlname=meetup_group_slug)
-                    nearby_venues = r.results
-                    [matched_venue] = [v for v in nearby_venues if v['name'] == row['venue_name']]
-                    # TODO: Search partial match?
-
-                # TODO: Fetch venue ID into spreadsheet? Might just be confusing.
-
-                # Set the event hosts from organizer/leadership group members.
-                r = mclient.GetProfiles(group_urlname=meetup_group_slug, role='leads')
-                lead_profiles = r.results
-                host_names = [h.strip().lower() for h in row['hosts'].split(',')]
-                # Match names with case-insensitivity.
-                host_profiles = [m for m in lead_profiles if m['name'].lower() in host_names]
-                # TODO: If 5 slots to used up, choose random organizers (based on criteria of recent attendance?)
+                if debug:
+                    click.echo('MATCHED MEETUP EVENT')
+                    click.echo(pprint.pformat(this_event))
+                    click.echo('SPREADSHEET EVENT DATA')
+                    click.echo(pprint.pformat(row))
 
                 event_data = {
                     'urlname': meetup_group_slug,
                     'id': this_event['id'],
                     'name': row['title_full'],
-                    'rsvp_limit': row['rsvp_limit'],
-                    'how_to_find_us': row['how_to_find_us'],
-                    'description': desc,
                     # Don't RSVP for the user who supplied API token.
                     'self_rsvp': False,
-                    'event_hosts': ','.join([str(m['member_id']) for m in host_profiles]),
-                    # TODO: Set to zero to remove ID if none provided. (Or set default.)
-                    'featured_photo_id': photo_id,
                 }
+
+                # If a field can be set by a simple string, allow it to be set
+                # from self-same spreadsheet column key.
+                API_STRING_FIELDS = ['rsvp_limit', 'how_to_find_us']
+                for key in row.keys():
+                    if row[key] and key in API_STRING_FIELDS:
+                        event_data[key] = row[key]
+
+                # Set event description from template
+                if row['template_url']:
+                    template_url = "{url}?r={cachebuster}".format(url=row['template_url'], cachebuster=script_time)
+                    r = requests.get(template_url)
+                    desc_tmpl = r.text
+                    # Pass spreadsheet dict into template func, do token replacement via header names.
+                    desc = pystache.render(desc_tmpl, row)
+                    # Add "**" to end of description to indicate this event as managed.
+                    desc = desc + "\n\n**"
+                    event_data['description'] = desc
+
+                if row['image_url']:
+                    # Download image from url.
+                    _, image_path = tempfile.mkstemp()
+                    r = requests.get(row['image_url'], allow_redirects=True)
+                    f = open(image_path, 'wb')
+                    f.write(r.content)
+                    featured_image = open(image_path, 'rb')
+
+                    # Get the group default album.
+                    r = mclient.GetPhotoAlbums(group_urlname=meetup_group_slug)
+                    [album] = [a for a in r.items if a['title'] == 'Meetup Group Photo Album']
+                    # Upload a photo to the default album.
+                    image_data = mclient.CreateGroupPhoto(await=True, group_urlname=meetup_group_slug, main=False, photo=featured_image)
+                    photo_id = image_data.group_photo_id
+                    # Set a caption to tag this photo as managed for this event.
+                    identifying_caption = 'Featured image for {date} event'.format(date=row['date'])
+                    new_photo = mclient.EditPhoto(id=photo_id, caption=identifying_caption)
+                    if debug: click.echo(pprint.pformat(new_photo.__dict__))
+                    # Delete existing photo that are tagged for this event.
+                    r = mclient.GetPhotos(photo_album_id=album['id'])
+                    marked_photos = [p for p in r.results if p.get('caption') == identifying_caption]
+                    # Skip the one we just uploaded if it's present.
+                    for photo in [p for p in marked_photos if p['photo_id'] != new_photo.photo_id]:
+                        r = mclient.DeletePhoto(id=photo['photo_id'])
+                        if debug: click.echo(">>> " + r.message)
+                    # TODO: Set to zero to remove ID if none provided. (Or set default.)
+                    event_data['featured_photo_id'] = photo_id
+
+                if row['venue_name']:
+                    r = mclient.GetVenues(group_urlname=meetup_group_slug)
+                    recent_venues = r.results
+                    venue = None
+                    # search prior venues for exact name match
+                    matched_venues = [v for v in recent_venues if row['venue_name'] == v['name']]
+                    if matched_venues:
+                        venue = matched_venues.pop()
+                    # search prior venues for partial name match
+                    if not venue:
+                        matched_venues = [v for v in recent_venues if row['venue_name'] in v['name']]
+                        if matched_venues:
+                            venue = matched_venues.pop()
+
+                    # search open public venues for matches
+                    # TODO: test this and enable
+                    feature_enabled = False
+                    if not venue and feature_enabled:
+                        r = mclient.GetOpenVenues(group_urlname=meetup_group_slug)
+                        nearby_venues = r.results
+                        [matched_venue] = [v for v in nearby_venues if v['name'] == row['venue_name']]
+                        # TODO: Search partial match?
+
+                    # TODO: Fetch venue ID into spreadsheet? Might just be confusing.
+
+                if row['hosts']:
+                    # Set the event hosts from organizer/leadership group members.
+                    r = mclient.GetProfiles(group_urlname=meetup_group_slug, role='leads')
+                    lead_profiles = r.results
+                    host_names = [h.strip().lower() for h in row['hosts'].split(',')]
+                    # Match names with case-insensitivity.
+                    host_profiles = [m for m in lead_profiles if m['name'].lower() in host_names]
+                    # TODO: If 5 slots to used up, choose random organizers (based on criteria of recent attendance?)
+                    host_ids = ','.join([str(m['member_id']) for m in host_profiles])
+                    event_data['event_hosts'] = host_ids
+
+                if debug: click.echo(">>> Updating event with current properties:\n" + pprint.pformat(event_data))
                 response = mclient.EditEvent(**event_data)
 
 if __name__ == '__main__':
