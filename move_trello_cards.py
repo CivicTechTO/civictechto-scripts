13d665034016c61e86693a3151d66e313b756daf
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-03-08 03:01:37 -0500

    move_trello_cards: Started using py-trello lib. Stubbed out a --stale-days flag.

--- move_trello_cards.py
+++ move_trello_cards.py
@@ -1,11 +1,17 @@
 import click
+import dateutil.parser
+import dateutil.tz
 import json
 import logging
 import os
 import re
 import requests
 
+from datetime import datetime, timedelta
+from trello import TrelloClient
+
 from commands.common import common_params
+from commands.utils.trello import BreakoutGroup
 
 
 CARD_IGNORE_LIST = os.getenv('TRELLO_CARD_IGNORE_LIST').split(',')
@@ -37,55 +43,59 @@ CONTEXT_SETTINGS = dict(help_option_names=['--help', '-h'])
               default='EVvNEGK5',
               help='ID of Trello board on which to act.',
               )
+@click.option('--stale-days', '-d',
+              default=0,
+              help='If provided, card not moved unless stale for this many days.',
+              )
 @common_params
-def move_trello_cards(api_key, api_secret, from_list, to_list, board, yes, verbose, debug, noop):
+def move_trello_cards(api_key, api_secret, from_list, to_list, board, stale_days, yes, verbose, debug, noop):
     if debug: click.echo('>>> Debug mode: enabled')
     if noop: click.echo('>>> No-op mode: enabled (No operations affecting data will be run)')
+    if stale_days:
+        raise NotImplementedError
 
     board_id = board
 
-    data = {
-            'key': api_key,
-            'token': api_secret,
-            'bid': board_id,
-            }
+    client = TrelloClient(
+        api_key=api_key,
+        api_secret=api_secret,
+    )
 
-    url = 'https://api.trello.com/1/boards/{bid}/lists?key={key}&token={token}'.format(**data)
-    r = requests.get(url)
-
-    board_lists = r.json()
+    board = client.get_board(board_id)
+    board_lists = board.get_lists('open')
 
     def select_list(lists, filter_string):
         field = 'id' if re.match('^[0-9a-f]+$', filter_string) else 'name'
-        [board_list] = [l for l in lists if l[field] == filter_string]
+        [board_list] = [l for l in lists if vars(l)[field] == filter_string]
         return board_list
 
     from_list = select_list(board_lists, from_list)
     to_list = select_list(board_lists, to_list)
 
-    data.update({'lid': from_list['id']})
-    url = 'https://api.trello.com/1/lists/{lid}/cards?key={key}&token={token}'.format(**data)
-    r = requests.get(url)
-    cards = r.json()
+    cards = from_list.list_cards()
 
     template = 'Moving cards from list "{}" to "{}"...'
-    click.echo(template.format(from_list['name'], to_list['name']), err=True)
+    click.echo(template.format(from_list.name, to_list.name), err=True)
 
     for c in cards:
-        if c['name'] in CARD_IGNORE_LIST:
+        if c.name in CARD_IGNORE_LIST:
             continue
 
-        data.update({'cid': c['id']})
-        url = 'https://api.trello.com/1/cards/{cid}?key={key}&token={token}'.format(**data)
+        date = c.dateLastActivity
+        delta = timedelta(days=stale_days)
+        now = datetime.now(dateutil.tz.tzutc())
+        if debug:
+            print(c.name)
+            print(date+delta < now)
 
         if not noop:
-            r = requests.put(url, data = {'idList': to_list['id']})
+            c.change_list(to_list.id)
 
         if verbose:
-            click.echo('Moved card: ' + c['name'], err=True)
+            click.echo('Moved card: ' + c.name, err=True)
 
         if debug:
-            click.echo('Card data: ' + json.dumps(c), err=True)
+            click.echo(vars(c), err=True)
 
     if noop: click.echo('Command exited no-op mode without creating/updating any data.')
 
