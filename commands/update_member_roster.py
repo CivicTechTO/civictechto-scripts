6e98d6fbaf8cf6a7aca479fc793503dd82d8601e
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-04-15 16:36:54 -0400

    Added logic for appending new members to spreadsheet. Reduced queries.

--- commands/update_member_roster.py
+++ commands/update_member_roster.py
@@ -90,51 +90,65 @@ def update_member_roster(gsheet, channel, slack_token, yes, verbose, debug, noop
 
     members = sc.get_user_members(channel['id'])
 
+    insert_count = 0
     for m in members:
+        # TODO: Confirm that this is what ends up being displayed in Slack.
+        username = m['profile']['display_name_normalized'] if m['profile']['display_name_normalized'] else m['profile']['real_name_normalized']
+        data = {}
+        data['first_name']     = m['profile'].get('first_name', '')
+        data['last_name']      = m['profile'].get('last_name', '')
+        data['slack_id']       = m['id']
+        data['slack_username'] = username
+        data['avatar_url']     = m['profile']['image_192']
+
         try:
-            match = wsheet.find(m['id'])
+            match = next(c for c in cell_list if c.value == m['id'])
+        except StopIteration:
+            match = None
+
+        if match:
+            # Update existing member row
+            is_locked = lambda c: c.value.startswith('lock:')
+            SKIP_VALUES = ['pass', 'skip', 'none']
+            is_skippable = lambda c: c.value.lower() in SKIP_VALUES
+
+            cells_to_update = []
+
             row_cells = [c for c in cell_list if c._row == match._row]
-        except CellNotFound:
-            print('TODO: Add row creation logic')
-            print(m)
-            continue
+            for i, c in enumerate(row_cells):
+                if is_locked(c):
+                    continue
 
-        if 'profile' in m:
-            # TODO: Confirm that this is what ends up being displayed in Slack.
-            username = m['profile']['display_name_normalized'] if m['profile']['display_name_normalized'] else m['profile']['real_name_normalized']
-            data = {}
-            data['first_name']     = m['profile'].get('first_name', '')
-            data['last_name']      = m['profile'].get('last_name', '')
-            data['slack_id']       = m['id']
-            data['slack_username'] = username
-            data['avatar_url']     = m['profile']['image_192']
+                if is_skippable(c):
+                    continue
 
-        is_locked = lambda c: c.value.startswith('lock:')
-        SKIP_VALUES = ['pass', 'skip', 'none']
-        is_skippable = lambda c: c.value.lower() in SKIP_VALUES
+                header = headers[i]
+                if header not in data:
+                    continue
 
-        cells_to_update = []
-        for i, c in enumerate(row_cells):
-            if is_locked(c):
-                continue
+                # If value is unchanged
+                if c.value == data[header]:
+                    continue
 
-            if is_skippable(c):
-                continue
+                c.value = data[header]
 
-            header = headers[i]
-            if header not in data:
-                continue
+                cells_to_update.append(c)
 
-            # If value is unchanged
-            if c.value == data[header]:
-                continue
-
-            c.value = data[header]
-
-            cells_to_update.append(c)
-
-        for c in cells_to_update:
-            wsheet.update_cell(c._row, c._col, c.value)
+            for c in cells_to_update:
+                wsheet.update_cell(c._row, c._col, c.value)
+        else:
+            # Create new member row
+            row_values = []
+            for h in headers:
+                row_values.append(data.get(h, ''))
+            range = "'{}'!{row}:{row}".format(wsheet.title, row=row_count+1+insert_count)
+            params = {'valueInputOption': 'RAW'}
+            body = {'values': [row_values]}
+            # NOTE: append and insert endpoints in the Google Sheets API seem to have a bug, so using a hack a update for now.
+            # See: https://github.com/burnash/gspread/issues/551_
+            # TODO: Accomodate when no extra rows in table.
+            wsheet.spreadsheet.values_update(range, params, body)
+            insert_count += 1
 
 if __name__ == '__main__':
     update_member_roster()
